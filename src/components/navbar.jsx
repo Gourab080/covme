ebc67038cd2983d80ebd5c693a6179ccc53cc31f
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-13 17:29:05 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,47 +11,26 @@ const navLinkProps = (path, animationDelay) => ({
 function Navbar({pages}) {
     const [expand, setExpand] = useState(false);
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark primary-color">
-        {/* Navbar brand */}
-        <a className="navbar-brand" href="#">Navbar</a>
-        {/* Collapse button */}
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        {/* Collapsible content */}
-        <div className="collapse navbar-collapse" id="basicExampleNav">
-          {/* Links */}
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home
-                <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-            {/* Dropdown */}
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div className="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
-          {/* Links */}
-          <form className="form-inline">
-            <div className="md-form my-0">
-              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-            </div>
-          </form>
+      <div id="custom-bootstrap-menu" className="navbar navbar-default " role="navigation">
+        <div className="container-fluid">
+          <div className="navbar-header"><a className="navbar-brand" href="#">Brand</a>
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-menubuilder"><span className="sr-only">Toggle navigation</span><span className="icon-bar" /><span className="icon-bar" /><span className="icon-bar" />
+            </button>
+          </div>
+          <div className="collapse navbar-collapse navbar-menubuilder">
+            <ul className="nav navbar-nav navbar-left">
+              <li><a href="/">Home</a>
+              </li>
+              <li><a href="/products">Products</a>
+              </li>
+              <li><a href="/about-us">About Us</a>
+              </li>
+              <li><a href="/contact">Contact Us</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        {/* Collapsible content */}
-      </nav>
+      </div>
     );
 }
 
