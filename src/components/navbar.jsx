938968e9e930b6c7375337fa6b1f03384f480d63
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-13 17:36:10 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,26 +11,23 @@ const navLinkProps = (path, animationDelay) => ({
 function Navbar({pages}) {
     const [expand, setExpand] = useState(false);
     return (
-      <div id="custom-bootstrap-menu" className="navbar navbar-default " role="navigation">
-        <div className="container-fluid">
-          <div className="navbar-header"><a className="navbar-brand" href="#">Brand</a>
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-menubuilder"><span className="sr-only">Toggle navigation</span><span className="icon-bar" /><span className="icon-bar" /><span className="icon-bar" />
-            </button>
-          </div>
-          <div className="collapse navbar-collapse navbar-menubuilder">
-            <ul className="nav navbar-nav navbar-left">
-              <li><a href="/">Home</a>
-              </li>
-              <li><a href="/products">Products</a>
-              </li>
-              <li><a href="/about-us">About Us</a>
-              </li>
-              <li><a href="/contact">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+        <nav className="flex items-center flex-wrap bg-white text-primary py-2">
+            <div className="container py-2 px-2 items-center font-bold text-lg">
+                <div className="w-full flex items-center justify-between -my-2">
+                    <NavLink exact={true} to='/' className="flex items-center cursor-pointer lg:flex">
+                        <img
+                            src="/images/covid.svg"
+                            alt="logo"
+                            className="h-10 w-auto mr-3"
+                        />
+                        <div className="text-xl font-bold text-black text-primary">
+                            COVID-19 India
+                        </div>
+                    </NavLink>
+                    
+                </div>
+            </div>
+        </nav>
     );
 }
 
