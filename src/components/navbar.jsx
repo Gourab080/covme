ff070e150d2ee75e3499e956863818c7c1753aea
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-13 18:11:05 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,63 +1,65 @@
-import React, { Component } from 'react';
-import { MDBHamburgerToggler, MDBNavbarToggler } from 'mdbreact';
-import {
-  MDBNavbar,
-  MDBNavbarBrand,
-  MDBNavbarNav,
-  MDBNavItem,
-  MDBNavLink,
-  MDBNavbarToggler,
-  MDBCollapse,
-  MDBContainer
-} from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-class Navbar extends Component {
-  state = {
-    collapseID: ''
-  };
+const navLinkProps = (path, animationDelay) => ({
+    className: `${window.location.pathname === path ? 'focused' : ''}`,
+    style: {
+        animationDelay: `${animationDelay}s`,
+    },
+});
 
-  toggleCollapse = collapseID => () => {
-    this.setState(prevState => ({
-      collapseID: prevState.collapseID !== collapseID ? collapseID : ''
-    }));
-  };
-
-  render() {
+function Navbar({pages}) {
+    const [expand, setExpand] = useState(false);
     return (
-        <MDBContainer>
-          <MDBNavbar
-            color='light-blue lighten-4'
-            style={{ marginTop: '20px' }}
-            light
-          >
-            <MDBContainer>
-              <MDBNavbarBrand>Navbar</MDBNavbarBrand>
-              <MDBNavbarToggler
-                onClick={this.toggleCollapse('navbarCollapse1')}
-              />
-              <MDBCollapse
-                id='navbarCollapse1'
-                isOpen={this.state.collapseID}
-                navbar
-              >
-                <MDBNavbarNav left>
-                  <MDBNavItem active>
-                    <MDBNavLink to='#!'>Home</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to='#!'>Link</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to='#!'>Profile</MDBNavLink>
-                  </MDBNavItem>
-                </MDBNavbarNav>
-              </MDBCollapse>
-            </MDBContainer>
-          </MDBNavbar>
-        </MDBContainer>
+        <nav className="flex items-center flex-wrap bg-white text-primary py-2">
+            <div className="container py-2 px-2 items-center font-bold text-lg">
+                <div className="w-full flex items-center justify-between -my-2">
+                    <NavLink exact={true} to='/' className="flex items-center cursor-pointer lg:flex">
+                        <img
+                            src="/images/covid.svg"
+                            alt="logo"
+                            className="h-10 w-auto mr-3"
+                        />
+                        <div className="text-xl font-bold text-black text-primary">
+                            COVID-19 India
+                            <span className="text-red-600 uppercase live-txt blink">
+                                Live
+                            </span>
+                        </div>
+                    </NavLink>
+                    <div className="flex ml-auto mr-1">
+                        {pages.map((page, i) => {
+                            if (page.showInNavbar === true) {
+                                return (
+                                    <NavLink
+                                        exact={true}
+                                        to={page.pageLink}
+                                        key={i}
+                                        className={
+                                            'mx-1 px-3 py-4 hidden lg:block nav-link'
+                                        }
+                                        activeClassName={
+                                            'relative nav-link-active'
+                                        }
+                                    >
+                                        <span
+                                            {...navLinkProps(
+                                                page.pageLink,
+                                                page.animationDelayForNavbar
+                                            )}
+                                        >
+                                            {page.displayName}
+                                        </span>
+                                    </NavLink>
+                                );
+                            }
+                            return null;
+                        })}
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
-  }
 }
 
 export default Navbar;
