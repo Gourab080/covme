b48a558b00a3ae8046cdee943986cf4eace4902b
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-13 20:32:50 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,16 +10,11 @@ const navLinkProps = (path, animationDelay) => ({
 
 function Navbar({pages}) {
     const [expand, setExpand] = useState(false);
-    return (
-              {/*Navbar*/}
-      <nav className="navbar navbar-light light-blue lighten-4">
-        {/* Navbar brand */}
+    return (  
+<nav className="navbar navbar-light light-blue lighten-4">
         <a className="navbar-brand" href="#">Navbar</a>
-        {/* Collapse button */}
         <button className="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span className="dark-blue-text"><i className="fas fa-bars fa-1x" /></span></button>
-        {/* Collapsible content */}
         <div className="collapse navbar-collapse" id="navbarSupportedContent1">
-          {/* Links */}
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
               <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
@@ -31,11 +26,8 @@ function Navbar({pages}) {
               <a className="nav-link" href="#">Pricing</a>
             </li>
           </ul>
-          {/* Links */}
         </div>
-        {/* Collapsible content */}
       </nav>
-      {/*/.Navbar*/}
     );
 }
 
