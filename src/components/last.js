12ac840de48112cdd37fd14bfb05e8caf5ba74ad
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-14 17:50:39 +0530

    Update last.js

--- src/components/last.js
+++ src/components/last.js
@@ -1,11 +1,30 @@
 import React from 'react';
-// import SocialCard from './social';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function Last() {
+ classes = useStyles();
     return (
-        <footer className="flex justify-center my-10 flex-col items-center">
-            <h3>Hello</h3>
-        </footer>
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                   <Paper className={classes.paper}>xs=12
+                   </Paper>
+                </Grid>
+               </Grid>
+             </div>
     );
 }
 
