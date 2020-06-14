fdec8dea6c4f995ca0602749192bb7ff2743f7a7
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-14 17:54:42 +0530

    Update last.js

--- src/components/last.js
+++ src/components/last.js
@@ -3,24 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
 function Last() {
- classes = useStyles();
     return (
-        <div className={classes.root}>
+        <div className="row">
             <Grid container spacing={3}>
               <Grid item xs={12}>
-                   <Paper className={classes.paper}>xs=12
+                   <Paper className="grv">xs=12
                    </Paper>
                 </Grid>
                </Grid>
