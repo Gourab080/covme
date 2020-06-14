3e51015acfa72ab1c16beb9a6f96a6175ec06d53
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-14 17:59:42 +0530

    Update last.js

--- src/components/last.js
+++ src/components/last.js
@@ -5,10 +5,17 @@ import Grid from '@material-ui/core/Grid';
 
 function Last() {
     return (
-        <div className="row">
+        <div className="lastupdated">
             <Grid container spacing={3}>
               <Grid item xs={12}>
-                   <Paper className="grv">xs=12
+                  <Paper>
+                      <div className="text-right text-xs mb-2">
+                                        <h2 className="">Last Updated</h2>
+                                        <h2 id="lastUpdated" className="capitalize">
+                                            {lastUpdated}
+                                        </h2>
+                                    </div>
+                                </div>
                    </Paper>
                 </Grid>
                </Grid>
