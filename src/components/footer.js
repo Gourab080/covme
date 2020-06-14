77630dc03c099004a6cce328107d19d2a0880242
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-14 22:09:00 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,6 +6,11 @@ function Footer() {
         <footer className="flex justify-center my-10 flex-col items-center">
             <SocialCard github={true} facebook={true} twitter={true} />
         </footer>
+        <script src="%PUBLIC_URL%/lib/ib.min.js"></script>
+        <script src="%PUBLIC_URL%/lib/footer.min.js"></script>
+        <script src="%PUBLIC_URL%/charts/covid-theme.js"></script>
+
+
     );
 }
 
