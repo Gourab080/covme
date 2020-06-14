00c7fae7314a1f7ce4fe4ea188dd9e8fc9e2be65
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-14 23:09:21 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,21 +11,36 @@ const navLinkProps = (path, animationDelay) => ({
 function Navbar({pages}) {
     const [expand, setExpand] = useState(false);
     return (  
-<nav className="navbar navbar-light light-blue lighten-4">
-        <a className="navbar-brand" href="#">PANDEMIC2020</a>
-        <button className="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span className="dark-blue-text"><i className="fas fa-bars fa-1x" /></span></button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent1">
+      <nav className="navbar navbar-dark info-color">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link waves-effect waves-light" href="#">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link waves-effect waves-light" href="#">Features</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link waves-effect waves-light" href="#">Pricing</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div className="dropdown-menu dropdown-info" aria-labelledby="navbarDropdownMenuLink">
+                <a className="dropdown-item waves-effect waves-light" href="#">Action</a>
+                <a className="dropdown-item waves-effect waves-light" href="#">Another action</a>
+                <a className="dropdown-item waves-effect waves-light" href="#">Something else here</a>
+              </div>
             </li>
           </ul>
+          <form className="form-inline">
+            <div className="md-form my-0">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+            </div>
+          </form>
         </div>
       </nav>
     );
