a67f0fcb24ec1106c6b82e055862d6fa1f31b86a
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-14 18:33:22 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,14 @@
 import React, { useState } from "react";
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBContainer
+} from 'mdbreact';
 import { NavLink } from "react-router-dom";
 
 const navLinkProps = (path, animationDelay) => ({
@@ -11,23 +21,37 @@ const navLinkProps = (path, animationDelay) => ({
 function Navbar({pages}) {
     const [expand, setExpand] = useState(false);
     return (  
-<nav className="navbar navbar-light light-blue lighten-4">
-        <a className="navbar-brand" href="#">PANDEMIC2020</a>
-        <button className="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span className="dark-blue-text"><i className="fas fa-bars fa-1x" /></span></button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent1">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+<MDBContainer>
+          <MDBNavbar
+            color='light-blue lighten-4'
+            style={{ marginTop: '20px' }}
+            light
+          >
+            <MDBContainer>
+              <MDBNavbarBrand>Navbar</MDBNavbarBrand>
+              <MDBNavbarToggler
+                onClick={this.toggleCollapse('navbarCollapse1')}
+              />
+              <MDBCollapse
+                id='navbarCollapse1'
+                isOpen={this.state.collapseID}
+                navbar
+              >
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to='#!'>Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to='#!'>Link</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to='#!'>Profile</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBContainer>
+          </MDBNavbar>
+        </MDBContainer>
     );
 }
 
