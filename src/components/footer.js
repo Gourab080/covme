b4695edb5dc8906eb075f2c75d63e830f5661bbf
Gourab <64215693+IamShakal@users.noreply.github.com>
2020-06-15 11:29:28 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as Icon from 'react-feather';
+// import * as Icon from 'react-feather';
 // import {useTranslation} from 'react-i18next';
 
 function Footer(props) {
